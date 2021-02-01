cb770cd3167e61e176af75ce601a4d6c7cd4bf40
Kang-Min-Seong <kdsvip5@naver.com>
2021-02-01 23:19:24 +0900

    02/01 2021 Day 7 + for statement on 2-dimensional array

--- programmers/Level 1/kThNumber.py
+++ programmers/Level 1/kThNumber.py
@@ -12,3 +12,17 @@ def solution(array, commands):
     return answer
 
 print(solution([1, 5, 2, 6, 3, 7, 4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
+
+'''
+    for문 사용 - 2차원 배열
+    
+    위 code 다른 풀이.
+    
+    for i in commands:
+        tmp = array[i[0]-1 : i[1]]
+        tmp.sort()
+        answer.append(tmp[i[2]-1])
+    
+    return answer
+        
+'''
