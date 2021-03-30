bfa5ba5b64b3c54457539995a48fa77f21bf3c08
Kang-Min-Seong <kdsvip5@naver.com>
2021-03-30 14:44:47 +0900

    03/30 Day 22 게임 맵 최단거리(gameMapShortestDistance)

--- programmers/Level 2/gameMapShortestDistance.py
+++ programmers/Level 2/gameMapShortestDistance.py
@@ -1,38 +1,41 @@
 # Programmers 03/25 2021
 # 게임 맵 최단거리
+from collections import deque
+
 
 def solution(maps):
     width = len(maps[0]) - 1
     height = len(maps) - 1
     move = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-    visit = [(0, 0)]
-    queue = [(0, 0, 1)]
-
-#    if maps[-1][-2] == 0 and maps[-2][-1] == 0:
-#       return -1
+    visit = {(0, 0)}
+    queue = deque()
+    queue.append((0, 0, 1))
 
     while queue:
-        tmp = queue.pop(0)
+        tmp = queue.popleft()
         y = tmp[0]
         x = tmp[1]
         check = tmp[2]
 
+        if y == height and x == width:
+            return check
+
         for my, mx in move:
             ny = y + my
             nx = x + mx
-            if 0 <= ny <= height and 0 <= nx <= width and maps[ny][nx] == 1 and (ny, nx) not in visit:
-                queue.append((ny, nx, check+1))
-                visit.append((ny, nx))
-
-                if ny == height and nx == width:
-                    return check + 1
+            set_size = len(visit)
+            if 0 <= ny <= height and 0 <= nx <= width and maps[ny][nx] == 1:
+                visit.add((ny, nx))
+                if set_size != len(visit):
+                    queue.append((ny, nx, check + 1))
 
     return -1
-
 
 print(solution([[1, 0, 1, 1, 1],
                 [1, 0, 1, 0, 1],
                 [1, 0, 1, 1, 1],
                 [1, 1, 1, 0, 1],
                 [0, 0, 0, 0, 1]]))
+
+
 
