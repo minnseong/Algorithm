d9a6e9acded4d5a72a11c182950e59d9ded29280
Kang-Min-Seong <kdsvip5@naver.com>
2021-06-29 19:12:47 +0900

    06/29 2021 BaekJoon 2606 Virus Solved

--- BaekJoon/Graph Traversal/2606_Virus.py
+++ BaekJoon/Graph Traversal/2606_Virus.py
@@ -3,40 +3,47 @@ connect_num = int(input())
 pair = []
 
 for _ in range(connect_num):
-    pair.append(input())
-
+    pair.append(input().split())
 graph = {}
 
 for p in pair:
     if p[0] not in graph:
-        graph[p[0]] = [p[2]]
+        graph[p[0]] = [p[1]]
     else:
-        graph[p[0]].append(p[2])
+        graph[p[0]].append(p[1])
 
 for p in pair:
-    if p[2] not in graph:
-        graph[p[2]] = [p[0]]
+    if p[1] not in graph:
+        graph[p[1]] = [p[0]]
     else:
-        graph[p[2]].append(p[0])
+        graph[p[1]].append(p[0])
 
-virus = graph['1']
-checked = []
+if '1' in graph:
+    virus = graph['1']
+    checked = []
 
-while True:
-    initSize = len(virus)
-    tmp = []
+    while True:
+        initSize = len(virus)
+        tmp = []
 
-    for v in virus:
-        if v in checked:
-            continue
-        checked.append(v)
-        if v in graph:
-            tmp.extend(graph[v])
+        for v in virus:
+            if v in checked:
+                continue
+            checked.append(v)
+            if v in graph:
+                tmp.extend(graph[v])
+        tmp = list(set(tmp))
+        virus.extend(tmp)
+        virus = list(set(virus))
 
-    virus.extend(tmp)
-    virus = list(set(virus))
+        if initSize == len(virus):
+            break
 
-    if initSize == len(virus):
-        break
+    if '1' in virus:
+        print(len(virus) - 1)
+    else:
+        print(len(virus))
+else:
+    print(0)
 
-print(len(virus)-1)
+
