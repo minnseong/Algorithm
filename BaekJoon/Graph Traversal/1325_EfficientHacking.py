8b30904fcef87bd71e693b95dabc4fe4c94ddb8f
Kang-Min-Seong <kdsvip5@naver.com>
2021-07-04 23:53:12 +0900

    07/04 2021 BaekJoon 1325 EfficientHacking Solving

--- BaekJoon/Graph Traversal/1325_EfficientHacking.py
+++ BaekJoon/Graph Traversal/1325_EfficientHacking.py
@@ -1,12 +1,10 @@
-import sys
 from collections import deque
+import sys
 
-input = sys.stdin.readline
-
-N, M = input().split()
+N, M = map(int, sys.stdin.readline().split())
 edge = []
-for _ in range(int(M)):
-    edge.append(list(map(int, input().split())))
+for _ in range(M):
+    edge.append(list(map(int, sys.stdin.readline().split())))
 
 graph = {}
 for g in edge:
@@ -15,10 +13,15 @@ for g in edge:
     else:
         graph[g[1]].append(g[0])
 
+check = [False for _ in range(N)]
+for i in graph:
+    check[i-1] = True
+
 
 def bfs(graph_, start, n):
     queue = deque([start])
-    visit = [False] * n
+    visit = [False for _ in range(n)]
+
     cnt = 0
 
     while queue:
@@ -26,17 +29,21 @@ def bfs(graph_, start, n):
         cnt += 1
         if not visit[node - 1]:
             visit[node - 1] = True
-            if node in graph_:
+            if check[node-1]:
                 queue.extend(graph_[node])
 
     return cnt
 
 
 cntList = []
-for i in range(int(M)):
-    cntList.append(bfs(graph, i + 1, int(N)))
+for i in range(N):
+    if check[i]:
+        cntList.append(bfs(graph, i + 1, N))
+    else:
+        cntList.append(0)
 
 maxCnt = max(cntList)
-for i in range(int(M)):
+
+for i in range(M):
     if cntList[i] == maxCnt:
         print(i+1, end=" ")
