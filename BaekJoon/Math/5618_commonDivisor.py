dbc51ea77c519173e4789e907458b06e8739f5bb
minnseong <kdsvip5@naver.com>
2021-08-24 01:19:32 +0900

    08/24 2021 BaekJoon 5618 CommonDivisor Solved

--- BaekJoon/Math/5618_commonDivisor.py
+++ BaekJoon/Math/5618_commonDivisor.py
@@ -1,19 +1,35 @@
 import sys
 
-input = sys.stdin.readline()
-N = int(input())
-num = input().split()
 
-ans = []
-num = list(map(int, num))
+def divisor(n, flag):
+    div = []
+    if flag == 1:
+        for i in range(1, (n//2) + 1):
+            if n % i == 0:
+                div.append(i)
 
-for i in range(1, min(num)):
+        div.append(n)
+        return div
+    elif flag == 2:
+        for i in range(1, (n//2) + 1):
+            if n % i == 0:
+                print(i)
+        print(n)
+
+
+N = int(sys.stdin.readline())
+num = list(map(int, sys.stdin.readline().split()))
+
+minValue = min(num)
+num.remove(minValue)
+minDivisor = reversed(divisor(minValue, 1))
+
+for m in minDivisor:
     cnt = 0
     for n in num:
-        if n % i == 0:
+        if n % m == 0:
             cnt += 1
     if cnt == len(num):
-        ans.append(i)
-
-for i in ans:
-    print(i)
+        ans = m
+        divisor(ans, 2)
+        break
