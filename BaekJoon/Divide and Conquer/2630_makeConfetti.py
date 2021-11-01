1eee581708ce59488a3b03fdfff1d27be62cf878
minnseong <kdsvip5@naver.com>
2021-11-01 23:34:38 +0900

    11/01 2021 BaekJoon 2630 makeConfetti Solved

--- BaekJoon/Divide and Conquer/2630_makeConfetti.py
+++ BaekJoon/Divide and Conquer/2630_makeConfetti.py
@@ -1,4 +1,4 @@
-# BaekJoon 10/30 2021
+# BaekJoon 11/01 2021
 # 2630 색종이 만들기
 
 N = int(input())
@@ -10,8 +10,44 @@ for i in range(N):
     confetti.append(list(map(int, input().split())))
 
 
-def countColor(arr):
-    return [arr.count(0), arr.count(1)]
+def checkConfetti(x, y, size):
+    wCnt, bCnt = 0, 0
+    global whiteCnt, blueCnt
+    for i in range(x, x+size):
+        for j in range(y, y+size):
+            if confetti[i][j] == 0:
+                wCnt += 1
+            elif confetti[i][j] == 1:
+                bCnt += 1
+            else:
+                return False
+
+            if bCnt > 0 and wCnt > 0:
+                return False
+
+    if bCnt == 0 and wCnt > 0:
+        whiteCnt += 1
+        return True
+    elif bCnt > 0 and wCnt == 0:
+        blueCnt += 1
+        return True
 
 
-print(confetti[0].count(1))
+def changeConfetti(x, y, size):
+    global confetti
+
+    for i in range(x, x+size):
+        for j in range(y, y+size):
+            confetti[i][j] = -1
+
+
+s = N
+while s >= 1:
+    for i in range(0, N, s):
+        for j in range(0, N, s):
+            if checkConfetti(i, j, s):
+                changeConfetti(i, j, s)
+    s //= 2
+
+print(whiteCnt)
+print(blueCnt)
