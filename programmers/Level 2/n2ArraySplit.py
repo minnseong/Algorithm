6c90e7d2abedfe2d91616c9cbee3afc33151bd8e
minnseong <kdsvip5@naver.com>
2021-12-11 22:01:30 +0900

    12/11 2021 Programmers n^2ArraySplit Solved

--- programmers/Level 2/n2ArraySplit.py
+++ programmers/Level 2/n2ArraySplit.py
@@ -1,32 +1,16 @@
-# Programmers 12/10 2021
+# Programmers 12/11 2021
 # n^2 배열 자르기
 
 def solution(n, left, right):
     answer = []
-    arr = [[0]*n for _ in range(n)]
 
-    for i in range(n):
-        tmp = 1+i
-        for j in range(n):
-            arr[i][j] = tmp
-            if tmp <= j+1:
-                tmp += 1
-    
-    # 시간초과로 2중 for문으로 2차원 배열을 1중 for문으로 2차원 배열을 만들려했지만 같음.
-    # tmp = 1
-    # for i in range(n*n):    
-    #     arr[int(i/n)][i%n] = tmp
-    #     if tmp <= i%n+1:
-    #         tmp += 1
-    #     if int((i+1)/n) != int(i/n):
-    #         tmp = int((i+1)/n) + 1
-
-    newArray = []
-    for i in range(n):
-        newArray.extend(arr[i])
-    
-    for i in range(left, right+1, 1):
-        answer.append(newArray[i])
+    for i in range(left, right+1):
+        a = int(i/n)
+        b = i%n
+        if a < b:
+            answer.append(b+1)
+        else:
+            answer.append(a+1)
 
     return answer
 
