476e31bf928407ccac2aad70260225c2b2f177da
minnseong <kdsvip5@naver.com>
2021-12-21 04:28:18 +0900

    12/21 2021 LeetCode Day 8  MergeTwo BinaryTrees Sd

---
+++ Leetcode/StudyPlanAlgorithm/MergeTwoBinaryTrees.py
@@ -0,0 +1,19 @@
+# LeetCode 2021 . 12 . 21
+# Study Plan - Algorithm Day 8 617. Merge Two Binary Trees
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def mergeTrees(self, root1: TreeNode, root2: TreeNode) -> TreeNode:
+        if root1 and root2:
+            node = TreeNode(root1.val + root2.val)
+            node.left = self.mergeTrees(root1.left, root2.left)
+            node.right = self.mergeTrees(root1.right, root2.right)
+            return node
+        else:
+            return root1 or root2
