5a614c93d6b91a53b991772a7ec217892b099e64
minnseong <kdsvip5@naver.com>
2022-01-21 15:17:29 +0900

    01/21 2022 Programmers H-index 코드 개선

--- programmers/Level 2/H-index.py
+++ programmers/Level 2/H-index.py
@@ -1,6 +1,20 @@
 # Programmers 01/21 2022
 # H-index
+# 시간복잡도 개선
 
+def solution(citations):
+    
+    citations.sort(reverse=True)
+
+    for i in range(len(citations)):
+        if citations[i] <= i:
+            return i
+    
+    return len(citations)
+
+print(solution([3, 0, 6, 1, 5]))
+
+'''
 def solution(citations):
     answer = []
     
@@ -17,5 +31,4 @@ def solution(citations):
             answer.append(h)
 
     return max(answer)
-
-print(solution([0,0,0,1]))
+'''
