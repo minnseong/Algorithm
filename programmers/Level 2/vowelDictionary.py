0a5baea8d9036f9a81ff88793cf603c78e293421
minnseong <kdsvip5@naver.com>
2022-01-23 23:17:46 +0900

    01/23 2022 Programmers 모음 사전 Solved

--- programmers/Level 2/vowelDictionary.py
+++ programmers/Level 2/vowelDictionary.py
@@ -1,18 +1,19 @@
-# Programmers 09/15 2021
+# Programmers 01/23 2022
 # 위클리 챌린지 - 5주차 모음사전
+
+from itertools import product
 
 # A E I O U
 def solution(word):
     answer = 0
-    wordToNum = {'A': '1', 'E': '2', 'I': '3', 'O': '4', 'U': '5'}
-    tmp = '0.'
+    word_ = ['A', 'E', 'I', 'O', 'U']
+    product_list = []
 
-    for w in word:
-        tmp += wordToNum[w]
+    for i in range(1, len(word_)+1):
+        product_list.extend(list(product(word_, repeat=i)))
+    
+    product_list.sort()
 
-    print(tmp)
+    return product_list.index(tuple(word))+1
 
-    return answer
-
-
-print(solution("AAAE"))
+print(solution("I"))
