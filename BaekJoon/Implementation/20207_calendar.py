e88e523d05ba3010713cad5429c196c4f5029fc5
minnseong <kdsvip5@naver.com>
2022-02-06 02:54:36 +0900

    02/06 2022 BaekJoon 20207 달력 Solved

--- BaekJoon/Implementation/20207_calendar.py
+++ BaekJoon/Implementation/20207_calendar.py
@@ -1,4 +1,4 @@
-# BaekJoon 02/05 2022
+# BaekJoon 02/06 2022
 # 20207 달력
 
 N = int(input())
@@ -6,28 +6,25 @@ calendar = [0]*365
 res = 0
 
 cl = [list(map(int, input().split())) for _ in range(N)]
-cl.sort(key=lambda x : (x[0], -x[1]))
 
-print(cl)
 for c in cl:
     s, e = c 
-    maxH = max(calendar[s-1:e])+1
     for i in range(s-1, e):
-        calendar[i] = maxH
+        calendar[i] += 1
 
-print(calendar)
 w, h = 0, 0
 i = 0
 while i < 365: 
     c = calendar[i]
+
     if c == 0:
         res += (w*h)
         w, h = 0 ,0
-
     else:
         w += 1
         h = max(h, c)
     
     i += 1
 
+res += (w*h)
 print(res)
