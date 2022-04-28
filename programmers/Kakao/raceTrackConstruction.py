6506d6db320553d05c923132eadc21a57402b744
minnseong <kdsvip5@naver.com>
2022-04-28 19:20:58 +0900

    04/28 2022 Programmers 경주로 건설 TestCase 25 fail

---
+++ programmers/Kakao/raceTrackConstruction.py
@@ -0,0 +1,44 @@
+# Programmers 04/28 2022
+# 경주로 건설
+
+from collections import deque
+
+def solution(board):
+
+    length = len(board)
+    queue = deque([[0, 0, [0, 0], 0]])
+    dir = [[1,0], [-1,0], [0,1], [0,-1]]
+    costs = [[0] * length for _ in range(length)]
+
+    while queue:
+        x, y, prevD, cost = queue.popleft()
+
+        for d in dir:
+            dx, dy = x+d[0], y+d[1]
+            if 0 <= dx < length and 0 <= dy < length:
+                if not board[dx][dy]:
+                    if dx == 0 and dy == 0:
+                        continue
+
+                    if x == 0 and y == 0:
+                        plusCost = 100
+                    else:
+                        if prevD[0] == d[0] and prevD[1] == d[1]:
+                            plusCost = 100
+                        else:
+                            plusCost = 600
+
+                    if costs[dx][dy] == 0:
+                        queue.append([dx,dy,d,cost+plusCost])
+                        costs[dx][dy] = cost+plusCost
+                    elif costs[dx][dy] >= cost+plusCost:
+                        queue.append([dx,dy,d,cost+plusCost])
+                        costs[dx][dy] = cost+plusCost  
+
+    return costs[-1][-1]
+
+print(solution([[0,0,0],[0,0,0],[0,0,0]]))
+print(solution([[0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 1, 1, 1, 1, 1, 0], [1, 0, 0, 1, 0, 0, 0, 0], [1, 1, 0, 0, 0, 1, 1, 1], [1, 1, 1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 1, 1, 1, 0], [1, 1, 1, 1, 1, 1, 1, 0], [1, 1, 1, 1, 1, 1, 1, 0]]))
+print(solution([[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 0, 1, 0, 0], [1, 0, 0, 0, 1], [0, 1, 1, 0, 0]]))
+print(solution([[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]]))
+print(solution([[0,0,0,0,0,0],[0,1,1,1,1,0],[0,0,1,0,0,0],[1,0,0,1,0,1],[0,1,0,0,0,1],[0,0,0,0,0,0]]))
