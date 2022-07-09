0e436925f0bd31c61fa51dfbdcff4f053809d6ca
minseong <kdsvip5@naver.com>
2022-07-09 17:22:22 +0900

    07/09 2022 BaekJoon 16916 부분 문자열 Solved

--- BaekJoon/String/161916_subString.py
+++ BaekJoon/String/161916_subString.py
@@ -1,6 +1,44 @@
-# BaekJoon 07/01 2022
+# BaekJoon 07/09 2022
 # 16916 부분 문자열
 
+def kmp_table(pattern):
+    length = len(pattern)
+    table = [0] * length
+    j = 0
+
+    for i in range(1, length):
+        while j > 0 and pattern[i] != pattern[j]:
+            j = table[j - 1]
+            
+        if pattern[i] == pattern[j]:
+            j += 1
+            table[i] = j
+
+    return table
+
+def kmp(parent, pattern):
+    table = kmp_table(pattern)
+    parent_length = len(parent)
+    pattern_length = len(pattern)
+
+    j = 0
+    for i in range(parent_length):
+        while j > 0 and parent[i] != pattern[j]:
+            j = table[j - 1]
+
+        if parent[i] == pattern[j]:
+            if j == pattern_length - 1:
+                return 1
+            else:
+                j += 1
+
+    return 0
+
+parent = input()
+pattern = input()
+print(kmp(parent, pattern))
+
+''' 시간 초과
 p = str(input())
 s = str(input())
 
@@ -17,11 +55,11 @@ else:
                     cnt += 1
                     i += 1
                 else:
-                    i -= 1
+                    i -= (j)
                     break
             if cnt == len(s):
                 ans = 1
                 break
         i += 1
     print(ans)
-
+'''
