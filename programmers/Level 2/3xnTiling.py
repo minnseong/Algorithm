c8bb4d966ec80cb4fde8e77daef1d5dfad1402e7
minseong <kdsvip5@naver.com>
2022-08-03 22:51:49 +0900

    08/03 2022 Programmers 3 x n 타일링 re Solved

--- programmers/Level 2/3xnTiling.py
+++ programmers/Level 2/3xnTiling.py
@@ -1,13 +1,24 @@
-# Programmers 07/22 2022
+# Programmers 08/03 2022
 # 3 x n 타일링
 
 def solution(n):
-    dp = [0] * (n + 1)
+    dp = [0] * (n+1)
+
     dp[0] = 1
-    tmp = 0
+    dp[2] = 3
 
-    for i in range(2, n + 1, 2):
-        dp[i] = (dp[i - 2] * 3) + (tmp * 2)
-        tmp += dp[i - 2]
+    if n <= 2:
+        return dp[n]
+        
+    for i in range(4, n+1, 2):
+        dp[i] = dp[i-2] * 4 - dp[i-4]
 
-    return dp[n] % 1000000007
+    return dp[-1] % 1000000007
+
+print(solution(4))
+
+
+# n = 0 -> 0
+# n = 2 -> 3
+# n = 4 -> 11
+# n = 6 -> 41
