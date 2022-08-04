4c70a783de6539643fc737bb78d6004af0e75e20
minseong <kdsvip5@naver.com>
2022-08-04 23:56:36 +0900

    08/04 2022 BaekJoon 11265 끝나지 않는 파티 Solved

---
+++ BaekJoon/Shortest Path/11265_notFinishedParty.py
@@ -0,0 +1,21 @@
+# BaekJoon 08/04 2022
+# 11265 끝나지 않는 파티
+
+import sys
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+parties = [list(map(int,input().split())) for _ in range(N)]
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if parties[i][j] > parties[i][k] + parties[k][j]:
+                parties[i][j] = parties[i][k] + parties[k][j]
+
+for _ in range(M):
+    A, B, C = map(int,input().split())
+    if parties[A-1][B-1] <= C:
+        print('Enjoy other party')
+    else:
+        print('Stay here')
