039635e9aaa937b5c37f80d24633b7c771faf82a
minseong <kdsvip5@naver.com>
2022-08-13 22:38:33 +0900

    08/13 2022 BJ 21939 문제 추천 시스템 Version 1 Solved

--- BaekJoon/Data Structure2/21939_ProblemRecommendSystemV1.py
+++ BaekJoon/Data Structure2/21939_ProblemRecommendSystemV1.py
@@ -1,6 +1,5 @@
-# BaekJoon 08/12 2022
+# BaekJoon 08/13 2022
 # 21939 문제 추천 시스템 Version 1 
-# 100% Failed
 
 import sys
 import heapq
@@ -9,36 +8,33 @@ input = sys.stdin.readline
 N = int(input())
 min_h = []
 max_h = []
+dic = dict()
 
 for _ in range(N):
     num, level = map(int, input().split())
     heapq.heappush(min_h, [level, num])
     heapq.heappush(max_h, [-level, -num])
+    dic[num] = False
 
 M = int(input())
-solvedSet = set()
 for _ in range(M):
-    cmd = input().split()
-    if cmd[0] == "add":
-        heapq.heappush(min_h, [int(cmd[2]), int(cmd[1])])
-        heapq.heappush(max_h, [int(cmd[2])*(-1), int(cmd[1])*(-1)])
-        # if int(cmd[1]) in solvedSet:
-        #     solvedSet.remove(int(cmd[1]))
-    elif cmd[0] == "recommend":
+    cmd = input().strip().split()
+    if cmd[0] == "recommend":
         if cmd[1] == '1':
-            while True:
-                if max_h[0][1]*(-1) in solvedSet:
-                    heapq.heappop(max_h)
-                else:
-                    print(max_h[0][1]*(-1))
-                    break
-        elif cmd[1] == '-1':
-            while True:
-                if min_h[0][1] in solvedSet:
-                    heapq.heappop(min_h)
-                else:
-                    print(min_h[0][1])
-                    break
-    elif cmd[0] == "solved":
-        solvedSet.add(int(cmd[1]))
-
+            while dic[-max_h[0][1]]:
+                heapq.heappop(max_h)
+            print(max_h[0][1] * (-1))
+        else:
+            while dic[min_h[0][1]]:
+                heapq.heappop(min_h)
+            print(min_h[0][1])
+    elif cmd[0] == "add":
+        while dic[-max_h[0][1]]:
+            heapq.heappop(max_h)
+        while dic[min_h[0][1]]:
+            heapq.heappop(min_h)
+        dic[int(cmd[1])] = False
+        heapq.heappush(min_h, [int(cmd[2]), int(cmd[1])])
+        heapq.heappush(max_h, [-int(cmd[2]), -int(cmd[1])])
+    else:
+        dic[int(cmd[1])] = True
