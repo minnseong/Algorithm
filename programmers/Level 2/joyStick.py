74be9c6f3f0a721db18d37dde6fc4399c87ce78e
minseong <kdsvip5@naver.com>
2022-10-07 03:46:00 +0900

    10/07 2022 Programmers 조이 스틱 resolved

--- programmers/Level 2/joyStick.py
+++ programmers/Level 2/joyStick.py
@@ -1,63 +1,40 @@
-# Programmers 04/04 2022
+# Programmers 10/07 2022
 # 조이 스틱
 
-import copy
-
-def terminationCondition(visited):
-    flag = True
-    for v in visited:
-        if not v:
-            flag = False
-            break
-    return flag
-
-def maxIndexExcess(idx, length):
-    if idx == length:
-        return 0
-    else:
-        return idx
-
 def solution(name):
-    move = []
-    operationCnt = [min(ord(n)-ord('A'), ord('Z')-ord(n)+1) for n in name]
+    
+    moves = []    
+    for n in list(name):
+        moves.append(min(abs(ord(n)-65), abs(91-ord(n))))
+    
+    if len(moves) == 1:
+        return sum(moves)
+    
+    if moves.count(0) == 0:
+        return sum(moves) + len(moves) - 1
 
-    # visited, 커서, 방향, moveCnt
-    # 종료 조건이 0인 지점외에 다 거쳐서 지나갓다면??
-    def tracking(visited, cur, dir, moveCnt):
-        visited_ = copy.deepcopy(visited)
-
-        if terminationCondition(visited_):
-            move.append(moveCnt)
-            return;
-        if moveCnt > len(name):
-            return;
+    if moves.count(0) == len(moves):
+        return 0
+    
+    min_move = 1e9
+    for i in range(1, len(moves)):
+        right, left = 0, 0
         
-        while True:
-            visited_[cur] = True
-
-            if terminationCondition(visited_):
-                move.append(moveCnt)
-                break
-
-            if operationCnt[cur] != 0 and (operationCnt[cur-1] == 0 or operationCnt[maxIndexExcess(cur+1, len(name))] == 0):
-                moveCnt += 1
-                tracking(visited_, cur+1, 1, moveCnt)
-                tracking(visited_, cur-1, -1, moveCnt)
-                return;
-            else:
-                moveCnt += 1
-                if dir == 1:
-                    cur += 1
-                else:
-                    cur -= 1
-
-    visited = [False if c != 0 else True for c in operationCnt]
-    if 'A' not in name:
-        return sum(operationCnt) + len(name) - 1
-    
-    tracking(visited, 0, 1, 0)
-    tracking(visited, 0, -1, 0)
-    # print(move)
-    return sum(operationCnt) + min(move)
-    
-print(solution("JEROEN"))
+        tmp = 0
+        while tmp < i:
+            if moves[tmp] != 0:
+                right = tmp
+            tmp += 1
+        
+        tmp = len(moves)-1
+        while tmp >= i:
+            if moves[tmp] != 0:
+                left = tmp
+            tmp -= 1
+        left = len(moves) - left
+        if left == len(moves):
+            left = 0
+        
+        min_move = min(min_move, 2*right+left, 2*left+right)
+        
+    return sum(moves) + min_move
