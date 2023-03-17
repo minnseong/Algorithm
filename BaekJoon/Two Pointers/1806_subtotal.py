2bc77a16e86b3b7f7403fd7b5c6b1f4db9cb56d4
minseong <kdsvip5@naver.com>
2023-03-17 17:16:03 +0900

    03/17 2023 BaekJoon 1806 부분합 Solved

--- BaekJoon/Two Pointers/1806_subtotal.py
+++ BaekJoon/Two Pointers/1806_subtotal.py
@@ -1,30 +1,26 @@
-# BackJoon 12/30 2021
-# 1806 부분합
+# BackJoon 03/17 2023
+# 골드4 - 부분합
 
+import sys
 N, S = map(int, input().split())
 num = list(map(int, input().split()))
 
-i, j = 0, 1
-minLength = N
-subSum = [num[i], 1]
+i, j = 0, 0
+sub_sum = 0
+result = sys.maxsize
 
-while i != j:
-    if j < N and subSum[0] < S:
-        subSum[0] += num[j]
-        subSum[1] += 1
+while True:
+    if sub_sum >= S:
+        result = min(result, j-i)
+        sub_sum -= num[i]
+        i += 1
+    elif j == N:
+        break
+    else:
+        sub_sum += num[j]
         j += 1
 
-    else:
-        if subSum[0] >= S and minLength > subSum[1]:
-            minLength = subSum[1]
-            if minLength == 1:
-                break
-        subSum[0] -= num[i]
-        subSum[1] -= 1
-        i += 1
-    
-    if i == 0 and j == N and subSum[0] < S:
-        minLength = 0
-        break
-
-print(minLength)
+if result == sys.maxsize:
+    print(0)
+else:
+    print(result)
