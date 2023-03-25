09be3de97396eabeb7ff9b8c0ef53421b12a8c22
minseong <kdsvip5@naver.com>
2023-03-25 20:54:08 +0900

    03/25 2023 BaekJoon 12865 평범한 배낭 Solved

--- BaekJoon/Dynamic Programming/12865_ plainBackpack.py
+++ BaekJoon/Dynamic Programming/12865_ plainBackpack.py
@@ -1,22 +1,23 @@
-# BaekJoon 09/11 2022
+# BaekJoon 03/25 2023
 # 12865 평범한 배낭
 
 import sys
-input = sys.stdin.readline
 
 N, K = map(int, input().split())
-items = [list(map(int, input().split())) for _ in range(N)]
+
+bags = []
+for _ in range(N):
+    bags.append(list(map(int, sys.stdin.readline().split())))
 
 dp = [[0] * (K+1) for _ in range(N+1)]
 
 for i in range(1, N+1):
     for j in range(1, K+1):
-        w = items[i-1][0]
-        v = items[i-1][1]
+        weight, value = bags[i-1][0], bags[i-1][1]
 
-        if j < w:
+        if j < weight:
             dp[i][j] = dp[i-1][j]
         else:
-            dp[i][j] = max(dp[i-1][j], v+dp[i-1][j-w])
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight] + value)
 
-print(dp[N][K])
+print(dp)        
